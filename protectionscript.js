0831b1fe433f77d222d81c7edc10e4ea8a5dfe0f
MattB1 <matthew_bubb@hotmail.com>
2021-09-04 20:04:40 -0400

    lower case

--- protectionscript.js
+++ protectionscript.js
@@ -3,8 +3,11 @@ let clue = document.getElementById("clue_text");
 clue.style.display = "none";
 let s = "";
 s = prompt("please enter the password");
-if(s == "clean"){
-    clue.style.display = "block";
-}else {
-    alert('Incorrect password-try again');
+if(s.length >= 1){
+    s = s.toLowerCase();
+    if(s == "clean"){
+        clue.style.display = "block";
+    }else {
+        alert('Incorrect password-try again');
+    }
 }
